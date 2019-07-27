af7d16fc132881778625156dac8e7abea702707f
velosos <thiagoveloso87@gmail.com>
2019-07-28 00:15:07 +0100

    fixed url of GA

--- lisboaiMain/core/static/js/ga.js
+++ lisboaiMain/core/static/js/ga.js
@@ -1,9 +1,6 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-142140480-1"></script>
-<script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'UA-142140480-1');
-</script>
+
