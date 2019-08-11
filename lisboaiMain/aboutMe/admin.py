fe100d9c942a3de3bffe0e1139cb78af3ddeba72
Thiago Veloso (ext.) <Thiago.Veloso@ctw.bmwgroup.com>
2019-08-11 23:59:14 +0100

    admin

--- lisboaiMain/aboutMe/admin.py
+++ lisboaiMain/aboutMe/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Profile,Social
+from .models import Profile,Social,Experience
 # Register your models here.
 
 
 admin.site.register(Profile)
 admin.site.register(Social)
+admin.site.register(Experience)
+
