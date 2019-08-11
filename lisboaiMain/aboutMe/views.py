41fd1986d16d9a1b63d8f024bf89c6a815802f71
Thiago Veloso (ext.) <Thiago.Veloso@ctw.bmwgroup.com>
2019-08-12 00:15:03 +0100

    ExperienceSerializer

--- lisboaiMain/aboutMe/views.py
+++ lisboaiMain/aboutMe/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Profile,Experience
-from .serializers import ProfileSerializer
+from .serializers import ProfileSerializer,ExperienceSerializer
 
 
 # Create your views here.
