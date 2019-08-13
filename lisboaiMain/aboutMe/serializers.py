39713aa2a420a1adc190d87d6519de4ff286b5f7
Thiago Veloso (ext.) <Thiago.Veloso@ctw.bmwgroup.com>
2019-08-13 10:49:50 +0100

    serialize

--- lisboaiMain/aboutMe/serializers.py
+++ lisboaiMain/aboutMe/serializers.py
@@ -7,7 +7,15 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = ('name', 'short_description', 'description', 'image', 'updated')
 
+
 class ExperienceSerializer(serializers.ModelSerializer):
+    user = ProfileSerializer()
     class Meta:
         model = Experience
         fields = ('user', 'title', 'company', 'logo', 'date','description')        
+
+        def create(self, validated_data):
+            user_data = validated_data.pop('user')  
+            user_ser = User.objects.create(**user_data)
+            experience = Experience.objects.create(user=user_ser, **user_data)
+            return experience
