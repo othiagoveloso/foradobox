786817f5fea133bb136f632a9c8f60263cad4705
Thiago Veloso <thiago.veloso@MacBook-Air-de-Thiago.local>
2020-05-25 22:30:54 -0300

    all

--- lisboaiMain/aboutMe/admin.py
+++ lisboaiMain/aboutMe/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile,Social,Experience, Skill, Education
+from .models import Profile,Social,Experience, Skill, Education, Certifications
 # Register your models here.
 
 
@@ -8,4 +8,6 @@ admin.site.register(Social)
 admin.site.register(Experience)
 admin.site.register(Skill)
 admin.site.register(Education)
+admin.site.register(Certifications)
+
 
